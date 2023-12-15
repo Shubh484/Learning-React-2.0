cd5a4032b43e95c71d4fdea6910139381d9ab327
Shubh <shubhsingh8767@gmail.com>
2023-12-15 16:52:26 +0530

    used css

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import "./index.css";
 
-function Greeting() {
+const BookList = () => {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <>
+      <section className="booklist">
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+      </section>
+    </>
   );
-}
+};
 
-const Person = () => <h2>Shubh Singh</h2>;
-const Message = () => {
-  return <p>this is my message</p>;
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-eu.ssl-images-amazon.com/images/W/MEDIAX_792452-T1/images/I/81uXd9NNVbL._AC_UL900_SR900,600_.jpg"
+    alt="The Magic (Tamil)"
+  />
+);
+const Title = () => <h2>The Magic (Tamil)</h2>;
+const Author = () => {
+  return <h4>Rhonda Byrne</h4>;
 };
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
