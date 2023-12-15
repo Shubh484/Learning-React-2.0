77c9d4f44e7703eaf01891a08e30cd8f980113df
Shubh <shubhsingh8767@gmail.com>
2023-12-15 11:58:34 +0530

    Day-1 of Learning Reactjs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Person = () => <h2>Shubh Singh</h2>;
+const Message = () => {
+  return <p>this is my message</p>;
+};
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(<Greeting />);
