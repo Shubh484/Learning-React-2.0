35270759dffda96c091b7a674a7db9b9c84b3787
Shubh <shubhsingh8767@gmail.com>
2023-12-17 17:21:09 +0530

    key props

--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@ const BookList = () => {
     <>
       <section className="booklist">
         {books.map((book) => {
-          const { img, title, author } = book;
-          return <Book img={img} title={title} author={author} />;
+          const { img, title, author, id } = book;
+          return <Book img={img} title={title} author={author} key={id} />;
         })}
       </section>
     </>
